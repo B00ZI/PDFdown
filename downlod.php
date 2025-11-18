f3d0061665c1548f848beef6c10db1c97c70709e
B00ZI <iraamaneyahya@gmail.com>
2025-11-18 23:25:48 +0100

    handling accesing the page from url amd thats it

--- downlod.php
+++ downlod.php
@@ -3,10 +3,12 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 header("Content-type:application/pdf");
-header("Content-Disposition:attachment;filename=yahya iraamane cv.pdf'");
+header("Content-Disposition:attachment;filename=yahya iraamane cv.pdf");
 readfile("yahya iraamane cv.pdf");
 
 
+}else{
+header("Location:home.php");
 }
 
 
